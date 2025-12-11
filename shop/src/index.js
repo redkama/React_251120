9e85e74667924e4f089ac92ce4db0c35c6e6c56f
redkama <redkama9@gmail.com>
2025-12-11 19:02:07 +0900

    React Study

--- shop/src/index.js
+++ shop/src/index.js
@@ -3,12 +3,21 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import{ BrowserRouter} from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './store'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <Provider store= {store}>
+    <BrowserRouter basename='/fruit' 
+      future={{
+        v7_startTransition: true,
+        v7_relativeSplatPath: true,
+      }}
+    >
+      <App />
+    </BrowserRouter>
+  </Provider>
 
 );
 
