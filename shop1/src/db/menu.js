a3951fce9ef627cefd4d7790918605ed9310d3d2
redkama <redkama9@gmail.com>
2025-12-16 13:02:05 +0900

    React Study

---
+++ shop1/src/db/menu.js
@@ -0,0 +1,40 @@
+const menuData = [
+  {
+    img: process.env.PUBLIC_URL + "/img/img_menu001.jpg",
+    en: "TERI GARLIC ROAST",
+    ko: "데리갈릭 로스트",
+    desc: "짭조름한 간장소스가 입맛을 확 당기게 하는 메뉴"
+  },
+  {
+    img: process.env.PUBLIC_URL + "/img/img_menu002.jpg",
+    en: "SPICY ROAST",
+    ko: "스파이시 로스트",
+    desc: "매콤한 특제 소스로 중독성 강한 메뉴"
+  },
+  {
+    img: process.env.PUBLIC_URL + "/img/img_menu003.jpg",
+    en: "SOY SAUCE ROAST",
+    ko: "간장 로스트",
+    desc: "깊고 진한 간장 풍미의 정석"
+  },
+  {
+    img: process.env.PUBLIC_URL + "/img/img_menu004.jpg",
+    en: "GARLIC ROAST",
+    ko: "갈릭 로스트",
+    desc: "마늘 향이 살아있는 담백한 메뉴"
+  },
+  {
+    img: process.env.PUBLIC_URL + "/img/img_menu005.jpg",
+    en: "HOT PEPPER ROAST",
+    ko: "핫페퍼 로스트",
+    desc: "알싸한 고추의 매력적인 매운맛"
+  },
+  {
+    img: process.env.PUBLIC_URL + "/img/img_menu006.jpg",
+    en: "PLAIN ROAST",
+    ko: "플레인 로스트",
+    desc: "본연의 맛을 살린 기본 메뉴"
+  }
+];
+
+export default menuData;
